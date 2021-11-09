09db4adb62ad0166806794bbc265f840939a6172
zefrog666 <93910825+zefrog666@users.noreply.github.com>
2021-11-09 14:49:38 +0800

    Update link.js

--- link.js
+++ link.js
@@ -1,14 +1,7 @@
-/*
- * 將要隨機派發的網址放入下方（不需排序）
- *
- * 並請注意：
- * 1. 除最後一項外，其餘項目用逗號分隔
- * 2. 網址請用引號包起來
- */
-
 
 const link = [
 	'https://www.surveycake.com/s/ybbQp',
 	'https://www.surveycake.com/s/amLDp',
+	'https://www.surveycake.com/s/1v601'
 	
 ];
